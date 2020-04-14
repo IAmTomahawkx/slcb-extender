e369ab358c1137d42db8b32013c422b82355f9c0
Tomahawk <47765953+IAmTomahawkx@users.noreply.github.com>
2020-04-14 14:44:19 -0700

    V0.3.0 example update

--- examples/examples.py
+++ examples/examples.py
@@ -1,145 +1,213 @@
-##### A basic bot #####
-# note that this file is not intended to run, but to show how things work
-import chatbot
+### Examples (revised for V0.3.0)
+# note that you should always have a unique name for your extension folder.
+# here, i will assume its called "extension_example"
+import sys, os
+sys.path.append(os.path.dirname(__file__)) # add your folder to sys.path
+import extension_example as ext
 
-bot = chatbot.Bot("!") # requires one argument, the prefix.
+bot = ext.Bot()
+# note that the bot injects the required Tick, Init, and Execute functions, plus SettingsReload. Do not include these in your script yourself.
 
-def Init():
-    bot.init(Parent) # this is really the only place you need to have Parent, the rest of the time you can use chatbot.parent,
-    # to ease your linters anger.
-def Unload():
-    # You MUST delete your bot instance. ironpython does something weird where it will keep the instance alive through reloads,
-    # and reuse your bot instance. to prevent this, you must delete it by removing all alive references.
-    global bot
-    bot = None
+# Bot takes quite a few keyword arguments.
+# you can pass a custom Settings class with (more on settings at the bottom)
+class MySettings(ext.Settings):
+    mysetting = "hi"
 
-@bot.command()
-def hello(msg):
-    """
-    this command takes no parameters.
-    """
-    msg.reply("hi! Im a robot! I can see that you are {0} and have {1} {2}".format(msg.author.name, msg.author.points, msg.bot.currencyname))
-    # you can access your settings through bot.settings, it will automatically pull data from your UI_Config,
-    # and settings.json . see the notes at the bottom about settings and streamlabs events.
-    some_setting = msg.bot.settings.mysetting
-    msg.reply("your setting: "+some_setting)
+ext.Bot(settings=MySettings) # note that you do not initialize the class
 
+# pass a different prefix by doing (defaults to !)
+ext.Bot(command_prefix="?")
 
-#### using settings to define a command name ####
-# here, it will pull from your UI_Config.json, and find the setting with the name youve given.
-# if the output_file doesnt exist, it will set the name to the default given in the UI_Config
+# enable debug on the bot (gives info on event dispatches and the like. also enables a special "dev" command group)
+ext.Bot(enable_debug=True)
 
-# lets imagine we have a UI_Config with a textbox named "welcome_command". note that this is NOT the textbox name.
+# the dev command group can be used by doing `!dev {subcommand}` in your chat.
+# the dev command currently has the `sudo` command, which runs another command, bypassing all checks and cooldowns
+# and the `su` command, which can be used to run a command as someone else.
 
-@bot.command("settings:welcome_command")
-def welcome_command(msg):
-    msg.reply(msg.command.name)
+##########
 
-
-#### here we start working with arguments ####
-
-@bot.command()
-def some_command(msg, firstarg=str, secondarg=int, rest=chatbot.RestOfInput(str)):
-    # arguments are split by spaces, multi-word input can be given by "quoting it".
-    # each argument MUST have a default, as the bot will convert each argument, and will fail to add the command
-    # if defaults are missing. if you miss a default, it will do something like:
-    # def thing(msg, argument=str, anotherargument, somethingelse=bool):
-    # will become
-    # def thing(msg, argument=str, anotherargument=bool, somethingelse=error raised!):
-    msg.reply(firstarg)
-
-#this expects input as such:
-#!some_command first_argument 5 anything past the number goes to the third argument!
-#passing a non-number to the number will raise an error. we will go over error handling later
-#!some_command hi hello otherstuff
-#will raise ConversionError, which subclasses UserInputError
-
-
-#### cooldowns and permissions ####
-
-# adding a per-user cooldown
-
-@chatbot.user_cooldown(1, per=5)
-@bot.command()
-def cooldown_example(msg):
-    """
-    a user can trigger me once every 5 seconds!
-    """
+# bot allows you to "listen" to events, such as on_init, on_message, etc (a full list can be found at further down.
+# as mentioned above, the bot injects the required Init, Tick, and Execute functions, plus SettingsReload. DO NOT implement them yourself!!
+# But for this to work properly, you must have at least *one* listener in your main script.
+@bot.listen()
+def on_init():
+    # do init stuff here
     pass
 
-# adding a global cooldown
+@bot.listen()
+def on_message(data):
+    # do message stuff here, but dont do commands here!
+    pass
 
-@chatbot.global_cooldown(1, per=5)
+
+##########
+
+# Bot also allows for commands, with full parameter parsing and cooldown support
+# by default the command is named after your function
+
 @bot.command()
-def global_cooldown_example(msg):
-    """
-    i can be triggered once every 5 seconds!
-    """
+def hello(msg): # triggered with !hello
     pass
 
-# requiring permissions
+# you can change this by doing
 
-@chatbot.check_editor()
-@bot.command("editor_required")
-def thingy(msg):
-    """
-    you must be an editor to use me!
-    """
+@bot.command("hello")
+def something(msg): # still triggered with !hello
     pass
 
-@chatbot.settings_permission("permissionSetting")
-def changable_permission(msg):
-    pass
-
-# custom checks
-@chatbot.check(lambda msg: return msg.author.name != "test_dummy")
+# you can apply cooldowns by doing
 @bot.command()
-def check_example(msg):
-    """
-    if the checks fail (including permissions checks), ChecksFailed will be raised, and will need handling (see below)
-    """
+@ext.user_cooldown(5, 1) # each user can only trigger this command once every 5 seconds
+def mycommand(msg):
+    pass
 
-#### error handling ####
-# there are 2 different error handlers. on_error and on_command_error
-# they are both events, and cannot have listeners (more than one function that recieves the event)
+# or
+@bot.command()
+@ext.global_cooldown(5, 1) #this can only be triggered once every 5 seconds
+def mycommand(msg):
+    pass
 
-# command_error takes 2 arguments, the message object, and the error that was raised.
+# commands must take at least one argument (excluding `self` if in a class), this is the ext.Message object.
+# it contains some info on the user that ran the command, where it was invoked, etc
+@bot.command()
+def reply(msg):
+    msg.reply("I'm Alive!") # replies to where the command was invoked from
 
-@bot.event
-def on_command_error(msg, error):
-    if isinstance(error, chatbot.UserInputError):
-        return msg.reply("Bad Input!")
-    if isinstance(error, chatbot.ChecksFailed):
-        return msg.reply("You do not have permission to use this command!")
-    if isinstance(error, chatbot.CommandOnCooldown):
-        return msg.reply("This command is still on cooldown for {0} more seconds!".format(error.retry_after))
-    if isinstance(error, chatbot.CommandError): #other error messages
-        return msg.reply(error.message)
+# commands can also take parameters
+# all parameters must have defaults, as that is how they are processed.
+# parameters will be converted to the default's type, IE
 
-# error takes 1 argument, the error. this is for errors that happen anywhere other than commands.
+@bot.command()
+def parameters(msg, arg1=str, arg2=bool): # doing arg1="", arg2=True) etc. will also work
+    msg.reply(arg1 + str(arg2))
 
-@bot.event
-def on_error(error):
-    bot.log(ScriptName, "error! "+error.message)
+@bot.command()
+def moreparameters(msg, arg1=ext.User, arg2=ext.Optional[str]):
+    # defaulting to ext.User means that the bot will attempt to find a viewer with the given name, otherwise it will raise an error (see: error handling)
+    # using the ext.Optional default will try to give you the given type (in this case a string), but it if cannot be converted, it will become None.
+    pass
+
+@bot.command()
+def evenmoreparameters(msg, arg1=ext.Union[ext.User, str], arg2=ext.RestOfInput):
+    # the Union default can be passed multiple values, it will try, in order, to convert it to one of the given options,
+    # otherwise will raise UnionConversionError.
+    # the RestOfInput default *must* be the last argument, as it will be given the rest of the input from the user
+    pass
+
+#######
+
+# error handling
+
+# there are two types of error handlers in this library, on_error, and on_command_error.
+# on_error will be called when a listener raises an error.
+# on_command_error will be called when a command raises an error (including during parsing)
+
+@bot.listen()
+def on_error(error, traceback):
+    # do things with the exception and traceback
+    pass
+
+@bot.listen()
+def on_command_error(msg, error, tb):
+    # do things with the Message object, the error, and the traceback
+    import traceback
+    tb = "".join(traceback.format_exception(type(error), error, tb))
+    bot.log("error while running command {0}: {1}".format(msg.command.qualified_name, tb))
+    msg.reply("whoops! something happened!")
+
+############
+
+# stream events
+
+# stream events can be accessed by having a StreamlabsEventToken textbox in your UI_Config.json, which, when a token is passed,
+# will enable the event listener. here are the events it will produce
+
+@bot.listen()
+def on_event_connect():
+    # dispatched when the socket connects
+    pass
+
+@bot.listen()
+def on_event_disconnect():
+    # dispatched when the socket disconnects
+    pass
+
+@bot.listen()
+def on_follow(user):
+    # takes an ext.User as its only argument
+    pass
+
+@bot.listen()
+def on_cheer(user, message, amount):
+    # its not rocket science
+    pass
+
+@bot.listen()
+def on_gift_sub(user, gifter):
+    # gifter could be None
+    pass
+
+@bot.listen()
+def on_streak_sub(user, months, streak_months):
+    # still not rocket science
+    pass
+
+@bot.listen()
+def on_resub(user, months, tier):
+    # tier is either 1000, 2000, or 3000 (#blametwitch)
+    pass
+
+@bot.listen()
+def on_sub(user, tier):
+    pass
+
+@bot.listen()
+def on_donation(user, amount, currency):
+    # amount is a float, unsure what currency is (i cant test it as im not affilate nor do i have a viewerbase)
+    pass
+
+@bot.listen()
+def on_raid(raider_name, count):
+    # note that raider_name is the users name, not a User object
+    # also note that raids are parsed via IRC, not the socket
+    pass
+
+@bot.listen()
+def on_event_receive(sender, args):
+    # the direct websocket info, dispatched whenever an event is received from the websocket
+    pass
 
 
-### events ####
-# all of the twitch events can be recieved through the bot.
-# on_raid will always be recievable, as it is done via twitch IRC parsing (thanks for the regex, Kruiser8!).
-# but all others; on_streak_sub, on_resub, on_sub ,on_follow, on_bits, on_donation
-# are done through the streamlabs event socket, and require your user to have their stremalabs token in the settings.json,
-# the textbox MUST be named StreamlabsEventToken for the bot to detect it.
-# all events recieved through the event socket will be sent to on_event_recieve as well as their individual handlers.
+#### other events
 
-# events can be set 2 ways. bot.event and bot.listener(). you can have multiple listeners, but there can only be one event function.
+# on_init()
+# on_message(data)
+# on_tick()
+# on_settings_reload(settings)
+# on_raw_receive(irc_data)
 
-@bot.event
-def on_sub(username):
-    bot.stream.send(username + " has just subscribed!")
 
-# this is a full list of the events that exist right now, plus the ones listed above
-# on_init ()
-# on_unload ()
-# on_settings_reload (dict)
-# on_tick ()
-# on_message (data)
+### settings
+
+# settings are automatically pulled from your UI_Config.json/settings.json (or whatever your output_file is set to)
+# you can access them via
+bot.settings.FieldName
+# where FieldName is the key in your UI_Config
+
+#### nodes
+
+# nodes can be created to make your code nicer, or to have commands in other files and batch load them into your bot
+
+class MyNode(ext.Node):
+    def __init__(self, bot):
+        self.bot = bot
+        ext.Node.__init__(self)
+
+    @ext.command() # note that you do not use the bot.command decorator here
+    def mycommand(self, msg, parameters=str):
+        # do stuff
+        pass
+
+# and can be added to the bot using
+bot.add_node(MyNode(bot))
